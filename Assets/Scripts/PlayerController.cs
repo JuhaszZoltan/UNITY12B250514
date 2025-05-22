e76c0c426cb103f72bd38ab442d44edc96a7fd8b
Juhász Zoltán <JuhaszZoltanMate@yahoo.com>
2025-05-22 12:15:34 +0200

    jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,11 +3,18 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float _maxSpeed = 5f;
+    [SerializeField] private float _jumpHeight = 230f;
+    [SerializeField] private Transform _groundChecker;
+    [SerializeField] private LayerMask _groundLayer;
+    
+    private float _groundCheckRadius = 0.1f; 
 
     private Rigidbody2D _rigidbody;
     private Animator _animator;
 
     private bool _facingRight = true;
+    private bool _onGround = false;
+
 
     private void Start()
     {
@@ -15,8 +22,27 @@ public class PlayerController : MonoBehaviour
         _animator = GetComponent<Animator>();
     }
 
+    private void Update()
+    {
+        if (_onGround && Input.GetAxis("Jump") > 0)
+        {
+            _onGround = false;
+            _animator.SetBool("onGround", false);
+            _rigidbody.AddForce(new Vector2(0, _jumpHeight));
+        }
+    }
+
     private void FixedUpdate()
     {
+        _onGround = Physics2D.OverlapCircle(
+            _groundChecker.position,
+            _groundCheckRadius,
+            _groundLayer);
+
+        _animator.SetBool("onGround", _onGround);
+        _animator.SetFloat("vertSpeed", _rigidbody.linearVelocity.y);
+
+
         float move = Input.GetAxis("Horizontal");
 
         _animator.SetFloat("speed", Mathf.Abs(move));
